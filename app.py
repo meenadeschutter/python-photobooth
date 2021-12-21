af508f7c82d863754decaf9cce964bd749fb78b8
Meena De Schutter <meenadeschutter@gmail.com>
2021-12-20 22:27:13 -0500

    Need to change to opencv-python for imshow() use

--- app.py
+++ app.py
@@ -7,13 +7,16 @@ app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    text = ''
-    try:
-        import cv2
-        text = 'success' 
-    except:
-        text = 'fail'
-        pass
+    import cv2
+    vid = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = vid.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    vid.release()
+    cv2.destroyAllWindows()
     secret_key = app.config.get("SECRET_KEY")
     return f"{text}.\nThe configured secret key is {secret_key}."
 
