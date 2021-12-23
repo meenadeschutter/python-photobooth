4ae673aca7f7f48355a24c88dc1e63be5524d1d1
Meena De Schutter <meenadeschutter@gmail.com>
2021-12-22 21:55:37 -0500

    Try using cameratag addon to get video working in Heroku

--- app.py
+++ app.py
@@ -6,33 +6,6 @@ app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
-cam_index = 0
-camera = cv2.VideoCapture(cam_index)
-
-def gen_frames():  # generate frame by frame from camera
-    global cam_index
-    global camera
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success and cam_index < 10:
-            cam_index += 1
-            camera = cv2.VideoCapture(cam_index)
-            print(cam_index)
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
-
-@app.route('/video_feed')
-def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
 @app.route("/")
 def index():
     text = ""
