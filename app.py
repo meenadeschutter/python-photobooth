822b1935a824372c756a358a2a87298d1a7e87ba
Meena De Schutter <meenadeschutter@gmail.com>
2021-12-25 21:12:52 -0500

    Modify photobooth-start-screen

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import os
 import cv2
 from flask import Flask, Response, render_template, url_for
 
+
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
+
 
 @app.route("/")
 def index():
@@ -16,6 +18,7 @@ def index():
     
     secret_key = app.config.get("SECRET_KEY")
     return f"{text}.\nThe configured secret key is {secret_key}."
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
